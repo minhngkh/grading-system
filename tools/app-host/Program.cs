281caf450696487230c1906197f2d27cdeff0b64
Minh Nguyen <minhngkh@gmail.com>
2025-05-17 02:36:31 +0700

    feat(app-host): update to use data volume instead of persisting the containers

--- tools/app-host/Program.cs
+++ tools/app-host/Program.cs
@@ -1,34 +1,62 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("postgres")
-                    .WithLifetime(ContainerLifetime.Persistent)
-                    .WithPgAdmin();
+    .WithDataVolume("postgres-data");
+    // .WithPgAdmin(); // Use dbgate instead 
 
 var rubricDb = postgres.AddDatabase("rubricdb");
 var assignmentFlowDb = postgres.AddDatabase("assignmentflowdb");
 
+var configPath = Path.Combine(
+    builder.AppHostDirectory,
+    "config"
+);
+
+// builder.AddContainer("bytebase", "bytebase/bytebase")
+//     .WaitFor(postgres)
+//     // .WithVolume("/var/opt/bytebase")
+//     .WithHttpEndpoint(targetPort: 8080, name: "bytebase-ui")
+//     .WithArgs([
+//         "--disable-metric",
+//         "--disable-sample",
+//     ]);
+
+builder.AddContainer("dbgate", "dbgate/dbgate", "alpine")
+    .WithVolume("dbgate-data", "/root/.dbgate")
+    .WithHttpEndpoint(targetPort: 3000, name: "dbgate-ui")
+    .WithEnvironment(ctx =>
+    {
+        ctx.EnvironmentVariables["CONNECTIONS"] = "con1";
+        ctx.EnvironmentVariables["LABEL_con1"] = "postgres";
+        ctx.EnvironmentVariables["SERVER_con1"] = "postgres";
+        ctx.EnvironmentVariables["USER_con1"] = postgres.Resource.UserNameParameter?.Value ?? "postgres";
+        ctx.EnvironmentVariables["PASSWORD_con1"] = postgres.Resource.PasswordParameter.Value;
+        ctx.EnvironmentVariables["ENGINE_con1"] = "postgres@dbgate-plugin-postgres";
+    });
+
+
 var username = builder.AddParameter("rb-username", secret: true);
 var password = builder.AddParameter("rb-password", secret: true);
-var rabbitmq = builder.AddRabbitMQ("messaging", username, password, 5672)
-                        .WithManagementPlugin();
- 
-var blobs = builder.AddAzureStorage("storage")
-                        .RunAsEmulator(
-                            azurite =>{
-                                azurite.WithLifetime(ContainerLifetime.Persistent);
-                                azurite.WithBlobPort(27000);
-                            })
-                        .AddBlobs("submissions-store");
+var rabbitmq = builder.AddRabbitMQ("messaging")
+    .WithManagementPlugin();
 
-// After adding all resources, run the app...
+var blobs = builder.AddAzureStorage("storage")
+    .RunAsEmulator(
+        azurite =>
+        {
+            azurite.WithDataVolume("storage-azurite-data");
+        }
+    )
+    .AddBlobs("submissions-store");
+
 var rubricEngine = builder.AddProject<Projects.RubricEngine_Application>("rubric-engine")
-        .WithReference(rubricDb).WaitFor(rubricDb)
-        .WithReference(rabbitmq).WaitFor(rabbitmq);
+    .WithReference(rubricDb).WaitFor(rubricDb)
+    .WithReference(rabbitmq).WaitFor(rabbitmq);
 
 builder.AddProject<Projects.AssignmentFlow_Application>("assignmentflow-application")
-        .WithReference(assignmentFlowDb).WaitFor(assignmentFlowDb)
-        .WithReference(blobs).WaitFor(blobs)
-        .WithReference(rabbitmq).WaitFor(rabbitmq)
-        .WithReference(rubricEngine).WaitFor(rubricEngine);
+    .WithReference(assignmentFlowDb).WaitFor(assignmentFlowDb)
+    .WithReference(blobs).WaitFor(blobs)
+    .WithReference(rabbitmq).WaitFor(rabbitmq)
+    .WithReference(rubricEngine).WaitFor(rubricEngine);
 
 builder.Build().Run();
