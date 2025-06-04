c249636b4ec94a01656a653319b54a216bbec080
Minh Nguyen <minhngkh@gmail.com>
2025-06-04 07:50:07 +0700

    chore: remove example projects

--- examples/example-next-app/eslint.config.mjs
+++
@@ -1,4 +0,0 @@
-import { flatConfig as pluginNext } from '@next/eslint-plugin-next';
-import baseConfig from '../../eslint.config.mjs';
-
-export default baseConfig.append([pluginNext.recommended]);
