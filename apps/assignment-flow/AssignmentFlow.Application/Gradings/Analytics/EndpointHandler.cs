2d5432ced9aef9df752c1b926f3f1d1f0b5d4057
nhkhiem <nhkhiem21@clc.fitus.edu.vn>
2025-06-17 00:01:16 +0700

    Added grading analytics feature with new endpoints
    Introduces a new `EndpointHandler` class with endpoints for overall and specific grading summaries, both requiring authorization. Updates the `GradingAnalyticService` interface and implementation to support fetching grading analytics using Entity Framework. Defines new models for structured data return. Modifies `EndpointHandlers` to integrate the new mapping method and updates `ServiceCollectionExtensions` to register the service for dependency injection.

---
+++ apps/assignment-flow/AssignmentFlow.Application/Gradings/Analytics/EndpointHandler.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AssignmentFlow.Application.Gradings.Analytics;
+
+public static class EndpointHandler
+{
+    public static IEndpointRouteBuilder MapGradingAnalytics(this IEndpointRouteBuilder endpoint)
+    {
+        endpoint.MapGet("/summary", GetGradingsSummary)
+            .WithName("GradingsSummary")
+            .Produces<OverallGradingAnalytics>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest);
+
+        endpoint.MapGet("/{id}/summary", GetGradingSummary)
+            .WithName("GradingSummary")
+            .Produces<GradingAnalytics>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest);
+
+        return endpoint;
+    }
+
+    [Authorize]
+    private static async Task<IResult> GetGradingsSummary(
+        [FromServices] GradingAnalyticService gradingAnalyticService,
+        ClaimsPrincipal user,
+        CancellationToken cancellationToken)
+    {
+        var teacherId = user.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new ArgumentNullException(nameof(user), "Teacher id must have been provided in the claims.");
+
+        var summary = await gradingAnalyticService.GetOverallGradingAnalyticsAsync(
+            teacherId,
+            cancellationToken);
+
+        return TypedResults.Ok(summary);
+    }
+
+    [Authorize]
+    private static async Task<IResult> GetGradingSummary(
+        [FromRoute] string id,
+        [FromServices] GradingAnalyticService gradingAnalyticService,
+        ClaimsPrincipal user,
+        CancellationToken cancellationToken)
+    {
+        var teacherId = user.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new ArgumentNullException(nameof(user), "Teacher id must have been provided in the claims.");
+
+        var summary = await gradingAnalyticService.GetGradingAnalyticsAsync(
+            teacherId,
+            id,
+            cancellationToken);
+
+        return TypedResults.Ok(summary);
+    }
+}
--- apps/assignment-flow/AssignmentFlow.Application/Shared/ServiceCollectionExtensions.cs
+++ apps/assignment-flow/AssignmentFlow.Application/Shared/ServiceCollectionExtensions.cs
@@ -1,9 +1,12 @@
-﻿namespace AssignmentFlow.Application.Shared;
+﻿using AssignmentFlow.Application.Gradings.Analytics;
+
+namespace AssignmentFlow.Application.Shared;
 
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddShared(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
     {
+        services.AddTransient<IGradingAnalyticService, GradingAnalyticService>();
         return services;
     }
 }
