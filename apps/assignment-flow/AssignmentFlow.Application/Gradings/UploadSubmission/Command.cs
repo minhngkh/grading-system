a32517d8111fcb9791c188f59d5ec199bc1276dc
nhkhiem <nhkhiem21@clc.fitus.edu.vn>
2025-06-22 12:49:38 +0700

    Updated blob name construction in CommandHandler.

--- apps/assignment-flow/AssignmentFlow.Application/Gradings/UploadSubmission/Command.cs
+++ apps/assignment-flow/AssignmentFlow.Application/Gradings/UploadSubmission/Command.cs
@@ -55,17 +55,18 @@ public class CommandHandler(BlobServiceClient client)
         [EnumeratorCancellation] CancellationToken cancellationToken
     )
     {
-        var baseBlobName = $"{command.SubmissionReference}/";
+        var baseBlobPath = $"{aggregate.Id.Value}/{command.SubmissionReference}/";
+        var submissionBlobBasePath = $"{command.SubmissionReference}/";
 
         if (!SupportedZipMimeTypes.Contains(command.File.ContentType))
         {
             // Handle single file upload
             await using var stream = command.File.OpenReadStream();
-            var blobName = baseBlobName + command.File.FileName;
+            var blobName = baseBlobPath + command.File.FileName;
             var blob = container.GetBlobClient(blobName);
             await blob.UploadAsync(stream, cancellationToken);
 
-            yield return Attachment.New(blobName);
+            yield return Attachment.New(submissionBlobBasePath + command.File.FileName);
         }
         else
         {
@@ -90,7 +91,7 @@ public class CommandHandler(BlobServiceClient client)
                     continue;
                 }
 
-                var blobName = baseBlobName + entry.FullName;
+                var blobName = baseBlobPath + entry.FullName;
                 var blob = container.GetBlobClient(blobName);
 
                 await using var entryStream = entry.Open();
@@ -111,7 +112,7 @@ public class CommandHandler(BlobServiceClient client)
                     cancellationToken
                 );
 
-                yield return Attachment.New(blobName);
+                yield return Attachment.New(submissionBlobBasePath + command.File.FileName);
             }
         }
     }
