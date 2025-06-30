ae691688014797c50a4d57eccfa52a9e5800f9ce
nhkhiem <nhkhiem21@clc.fitus.edu.vn>
2025-06-30 22:23:15 +0700

    Added Grader property to ScoreBreakdownApiContract
    Enhance the ScoreBreakdownApiContract class by adding a new `Grader` property, initialized to an empty string. Update the `Status` property to also initialize to an empty string.
    
    Modify the `ScoreBreakdownsExtension` class to map the new `Grader` property correctly.
    
    Create a migration class `AddGrader` to update the database schema accordingly, and update the `AssignmentFlowDbContextModelSnapshot` to reflect these changes.
    
    These updates improve the tracking of who graded each score breakdown, enhancing the grading process's functionality and traceability.

---
+++ apps/assignment-flow/AssignmentFlow.Application/Migrations/20250630152107_AddGrader.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssignmentFlow.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGrader : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
